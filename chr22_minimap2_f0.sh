c89198b9d80938e549a5bf798f95650b2736ea04
Chu <chucl@hg-02035003-lm3.nih.gov>
2019-06-10 15:41:50 -0400

    testing f values command fixes

--- chr22_minimap2_f0.sh
+++ chr22_minimap2_f0.sh
@@ -6,9 +6,10 @@
 module load minimap2
 module load samtools
 
-f_val=0.0002
+f_val=$1
+input_path=/data/rhiea/chr22-sim/
 
-minimap2 -x map-ont -N50 -r30000 -a -t20  -f$f_val /data/rhiea/chr22-sim/chr22.fasta /data/rhiea/chr22-sim/chr22-a02-s10.fasta | samtools view -hb -@4 > minimap2_N50_r30kb.bam
-samtools sort -@20 -O cram --reference=chr22.fasta -T minimap2_N50_30kb.tmp minimap2_N50_30kb.bam > minimap2_N50_r30kb.cram
-samtools index minimap2_N50_30rkb.cram
-/home/rhiea/tools/IGVTools/igvtools count minimap2_N50_30kb.cram minimap2_N50_30kb.tdf chr22.fasta.fai
+minimap2 -x map-ont -N50 -r30000 -a -t20  -f$f_val ${input_path}chr22.fasta ${input_path}chr22-a02-s10.fasta | samtools view -hb -@4 > minimap2_N50_r30kb_${f_val}.bam
+samtools sort -@20 -O cram --reference=${input_path}chr22.fasta -T minimap2_N50_r30kb_${f_val}.tmp minimap2_N50_r30kb_${f_val}.bam > minimap2_N50_r30kb_${f_val}.cram
+samtools index minimap2_N50_r30kb_${f_val}.cram
+/home/rhiea/tools/IGVTools/igvtools count minimap2_N50_r30kb_${f_val}.cram minimap2_N50_30kb.tdf ${input_path}chr22.fasta.fai
